b3e65864618ca9d45841b26830edefbb9f4cd8e7
Askarbek Karasasev <askar.karasaev@gmail.com>
2016-11-23 22:28:49 +0900

    addding 2 more fields into events database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123130644) do
+ActiveRecord::Schema.define(version: 20161123132756) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20161123130644) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "starts_at"
+    t.text     "decription"
   end
 
 end
